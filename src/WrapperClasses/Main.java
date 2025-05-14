db544851ff8184ff3ed9342272c4f4d138c47eef
duedull <mr.duedull@gmail.com>
2025-05-14 12:41:01 +0530

    wrapper classes

---
+++ src/WrapperClasses/Main.java
@@ -0,0 +1,13 @@
+package WrapperClasses;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer a = 5;
+        Double b = 6.67890;
+        String c = "this can go wrong";
+        Short d = 3;
+        Boolean e = true;
+        String dstr = b.toString();
+        System.out.println(dstr.length());
+    }
+}
