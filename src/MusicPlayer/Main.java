5bd4c62989eba0f9e03db79dca1261ee98964403
duedull <mr.duedull@gmail.com>
2025-05-15 11:39:45 +0530

    incomplete music player

---
+++ src/MusicPlayer/Main.java
@@ -0,0 +1,14 @@
+package MusicPlayer;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    //to use mp3 files we have to import some libraries
+    String wavPath = "sci-fi-background-258999.wav";
+    File file = new File(wavPath);
+}
+}
