e727304d9fef99e8621b29a2ebc6b1ee2ccb4bc1
duedull <mr.duedull@gmail.com>
2025-05-16 21:17:58 +0530

    more of hashmaps

--- src/DataStructures/Hashmap.java
+++ src/DataStructures/Hashmap.java
@@ -8,9 +8,19 @@ public class Hashmap {
         Hm1.put("a","Abhay singh");
         Hm1.put("b","bhaskar");
         Hm1.put("c","catwave");
-        Hm1.put("d","dragon");
         Hm1.put("e","Esha");
+        Hm1.put("d","dragon");
 
         System.out.println(Hm1);
+        System.out.println(Hm1.get("c"));
+        Hm1.put("c","catman");
+        System.out.println(Hm1);
+        System.out.println(Hm1.containsKey("b"));
+        System.out.println(Hm1.containsValue("naga"));
+        System.out.println(Hm1.keySet());
+
+        for(String k: Hm1.keySet()){
+            System.out.println(k+ " : " + Hm1.get(k));
+        }
     }
 }
