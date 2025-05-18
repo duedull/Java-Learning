8adbf7e9d1a04473ef1306d78116b734f215bd8b
duedull <mr.duedull@gmail.com>
2025-05-18 21:50:24 +0530

    factorial programme

--- src/ZERO/Programs/Factorial.java
+++ src/ZERO/Programs/Factorial.java
@@ -1,7 +1,16 @@
 package ZERO.Programs;
+import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        System.out.println("t");
+        System.out.println("Programme to find the factorial of any number\nPlease enter any number: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int no = scanner.nextInt();
+        long temp = 1;
+        for(int i = no; i >= 1; i--){
+            temp = temp * i;
+        }
+        System.out.printf("%d! is %d",no,temp);
     }
 }
