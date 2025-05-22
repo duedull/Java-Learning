5bc2f14a420e228e8d6dec52205de67ec0c5192c
duedull <mr.duedull@gmail.com>
2025-05-22 13:54:12 +0530

    left pascal triangle program

--- src/temp.java
+++ src/temp.java
@@ -1,18 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+package ZERO.Programs.Patterns;
+
+import java.util.Scanner;
 
 public class temp {
-    public static void main(String[] args) {
-        try {
-            Process process = Runtime.getRuntime().exec("wmic memorychip get Manufacturer, PartNumber, Speed");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
 
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int row = scanner.nextInt();
+
+        // Upper half of Pascal Triangle
+        for (int i = 1; i <= row; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println();
+        }
+
+        // Lower half of Pascal Triangle
+        for (int i = row - 1; i > 0; i--) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
