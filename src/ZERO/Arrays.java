71c7d4be7198522c4b5d0161cabd3d9aca55650c
duedull <mr.duedull@gmail.com>
2025-05-27 23:53:13 +0530

    just array

---
+++ src/ZERO/Arrays.java
@@ -0,0 +1,15 @@
+package ZERO;
+
+import java.util.ArrayList;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        double[] arr;
+        arr   = new double[8];
+        arr[0] = 3.5;
+        arr[1] = 3.7;
+        arr[2] = 3.2;
+
+    }
+}
